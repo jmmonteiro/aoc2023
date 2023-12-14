aa4e8fbcd5803ed2748a44be4c037520a241f639
jmmonteiro <j.monteiro@heni.com>
2023-12-14 18:30:25 +0000

    Remove extra comments on day 13

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -1,10 +1,4 @@
 //! Point of Incidence
-//!
-//! To summarize your pattern notes, add up the number of columns to the left of each vertical
-//! line of reflection; to that, also add 100 multiplied by the number of rows above each
-//! horizontal line of reflection. In the above example, the first pattern's vertical line
-//! has 5 columns to its left and the second pattern's horizontal line has 4 rows above it,
-//! a total of 405.
 use crate::utils::structs::{Answer, Solver};
 use std::time::Instant;
 
